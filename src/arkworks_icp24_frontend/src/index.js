d3f5033ed1578d278fb0e2083978d782c5ae41e6
yugo <yuu5fujii@gmail.com>
2023-04-16 16:33:39 +0900

    残りはフロントの整理、consoleのエラーメッセージ、CSS入れる

--- src/arkworks_icp24_frontend/src/index.js
+++ src/arkworks_icp24_frontend/src/index.js
@@ -1,5 +1,5 @@
+import init, { create_proof, create_json_proof } from "./rust_zkp/pkg/rust_zkp.js";
 import { arkworks_icp24_backend } from "../../declarations/arkworks_icp24_backend";
-import init, { create_proof } from "./rust_zkp/pkg/rust_zkp.js";
 
 (async () => {
   await init();
@@ -17,12 +17,26 @@ document.querySelector("form").addEventListener("submit", async (e) => {
   // Generate proof
   const proof = create_proof(a, b);
 
-  // Interact with the backend canister, calling the verify_groth16 method with the generated proof
-  const greeting = await arkworks_icp24_backend.verify_groth16(proof);
+  // Generate JSON proof
+  const json_proof = create_json_proof(proof);
+  console.log("Proof:", proof);
+  console.log("JSON Proof:", json_proof);
+
+  try {
+    // Interact with the backend canister, calling the verify_groth16 method with the generated proof
+    const verificationResult = await arkworks_icp24_backend.verify_groth16(proof);
+    console.log("Verification Result:", verificationResult);
+
+    // Display the JSON proof
+    document.getElementById("json-proof").innerText = JSON.stringify(JSON.parse(json_proof), null, 2);
+
+    // Display the verification result
+    document.getElementById("verification-result").innerText = verificationResult === "Proof is valid" ? "Proof is valid!" : "Proof is invalid!";
+  } catch (error) {
+    document.getElementById("verification-result").innerText = "Error: " + error;
+  }
 
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
 
   return false;
 });
