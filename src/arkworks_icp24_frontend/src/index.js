ca59eef18687f866db740b31771650a9d40ecc49
yugo <yuu5fujii@gmail.com>
2023-04-17 11:26:07 +0900

    add comment

--- src/arkworks_icp24_frontend/src/index.js
+++ src/arkworks_icp24_frontend/src/index.js
@@ -15,30 +15,24 @@ document.querySelector("form").addEventListener("submit", async (e) => {
   button.setAttribute("disabled", true);
 
   const resultElement = document.getElementById("verification-result");
-  resultElement.style.display = "none"; // 追加: submitボタンが押された時にResultを消す
+  resultElement.style.display = "none"; 
 
-  // Generate proof
   const proof = create_proof(a, b);
 
-  // Generate JSON proof
   const json_proof = create_json_proof(proof);
 
-  // Display the JSON proof
   const jsonProofElement = document.getElementById("json-proof");
   jsonProofElement.innerText = JSON.stringify(JSON.parse(json_proof), null, 2);
-  jsonProofElement.style.display = "block"; // 追加: 証明が生成された時に表示を切り替える
+  jsonProofElement.style.display = "block"; 
   
-
   try {
-    // Interact with the backend canister, calling the verify_groth16 method with the generated proof
     const verificationResult = await arkworks_icp24_backend.verify_groth16(proof);
 
-    // Display the verification result
     resultElement.innerText = verificationResult === "Proof is valid" ? "Proof is True!" : "Proof is False";
-    resultElement.style.display = "inline"; // 結果が得られた時に表示を切り替える
+    resultElement.style.display = "inline"; 
   } catch (error) {
     resultElement.innerText = "Error: " + error;
-    resultElement.style.display = "inline"; // エラーが発生した時に表示を切り替える
+    resultElement.style.display = "inline"; 
   }
 
   button.removeAttribute("disabled");
@@ -46,13 +40,12 @@ document.querySelector("form").addEventListener("submit", async (e) => {
   return false;
 });
 
-// Add reset button event listener
 document.getElementById("reset-button").addEventListener("click", () => {
   document.getElementById("number1").value = "";
   document.getElementById("number2").value = "";
   document.getElementById("verification-result").innerText = "";
   const jsonProofElement = document.getElementById("json-proof");
-  jsonProofElement.innerText = ""; // 追加: JSON証明をリセットする
-  jsonProofElement.style.display = "none"; // 追加: JSON証明を非表示にする
+  jsonProofElement.innerText = ""; 
+  jsonProofElement.style.display = "none"; 
 });
 
